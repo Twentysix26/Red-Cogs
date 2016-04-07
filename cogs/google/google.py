f28d02dae9000f34bd52334a603b3032ea14f1d2
Kowlin <boxedpp@gmail.com>
2016-04-07 17:29:28 +0200

    Updates google.py
    Spoiler: WOOO!!!

--- cogs/google/google.py
+++ cogs/google/google.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from .utils import checks
+import urllib
 
 class SimplyGoogle:
     """A non sarcastic google command"""
@@ -10,10 +11,45 @@ class SimplyGoogle:
 
     @commands.command(pass_context=True, no_pm=True)
     async def google(self, ctx, text):
-        """I don't need to explain this"""
-        uri = "https://www.google.com/search?q="
-        quary =  str(ctx.message.content[len(ctx.prefix+ctx.command.name)+1:].replace(" ","+"))
-        await self.bot.say(uri+quary)
+        """Its google, you search with it.
+        Example: google A french pug
+
+        Special search options are avaiable; Image, Maps
+        Example: google image You know, for kids!
+        Another example: google maps New York"""
+        search_type = ctx.message.content[len(ctx.prefix+ctx.command.name)+1:].lower().split(" ")
+        #Start of Image
+        if search_type[0] == "image":
+            search_valid = str(ctx.message.content[len(ctx.prefix+ctx.command.name)+1:].lower())
+            if search_valid == "image":
+                await self.bot.say("Please actually search something")
+            else:
+                await self.bot.say(search_type)
+                uri = "https://www.google.com/search?tbm=isch&q="
+                quary = str(ctx.message.content[len(ctx.prefix+ctx.command.name)+7:].lower())
+                encode = urllib.parse.quote_plus(quary,encoding='utf-8',errors='replace')
+                await self.bot.say(uri+encode)
+            #End of Image
+        #Start of Maps
+        elif search_type[0] == "maps":
+            search_valid = str(ctx.message.content[len(ctx.prefix+ctx.command.name)+1:].lower())
+            if search_valid == "maps":
+                await self.bot.say("Please actually search something, even google cant fix your stupidity")
+            else:
+                await self.bot.say(search_type)
+                uri = "https://www.google.com/maps/search/"
+                quary = str(ctx.message.content[len(ctx.prefix+ctx.command.name)+6:].lower())
+                encode = urllib.parse.quote_plus(quary,encoding='utf-8',errors='replace')
+                await self.bot.say(uri+encode)
+            #End of Maps
+        #Start of generic search
+        else:
+            await self.bot.say(search_type)
+            uri = "https://www.google.com/search?q="
+            quary = str(ctx.message.content[len(ctx.prefix+ctx.command.name)+1:])
+            encode = urllib.parse.quote_plus(quary,encoding='utf-8',errors='replace')
+            await self.bot.say(uri+encode)
+            #End of generic search
 
 def setup(bot):
     n = SimplyGoogle(bot)
