6924392da91702bae876183f087b9450dd334dce
rfilkins1992 <rfilkins1992@users.noreply.github.com>
2016-04-15 12:14:01 -0500

    Update weather.py
    Added more commands that were written by Dex

--- cogs/weather/weather.py
+++ cogs/weather/weather.py
@@ -13,20 +13,83 @@ class weather:
 	def __init__(self, bot):
 		self.bot = bot
 		self.settings = fileIO("data/weather/settings.json", "load")
-
+		
 	@commands.command(no_pm=True, pass_context=False)
 	async def temp(self, location):
-		"""Make sure to get your own API key and put it into data/weather/settings.json
-		\nYou can get an API key from: www.wunderground.com/weather/api/"""
-		url = "http://api.wunderground.com/api/" + self.settings['api_key'] + "/conditions/q/" + location + ".json"
+		"""Get the current weather.
+		\nMake sure to get your own API key and put it into data/weather/settings.json\nYou can get an API key from: www.wunderground.com/weather/api/"""
+		url = "http://api.wunderground.com/api/" + self.settings['api_key'] + "/geolookup/conditions/q/" + str(location) + ".json"
 		async with aiohttp.get(url) as r:
-		    data = await r.json()
-		if "current_observation" in data:
-			temperature = data["current_observation"].get("temperature_string", "No temperature found.")
-			await self.bot.say("Current temperature in your location is: " + temperature +".")
-		else:
-			await self.bot.say("Please use your zip code or the format City,State(City,Country if outside of the US). EX: London,UK \n****NOTE:**** Include the ',' after the city and do NOT put a space after the ','")
+			data = await r.json()
 
+		if "current_observation" in data:
+			tempc = data["current_observation"].get("temp_c", "No temperature found.")
+			tempf = data["current_observation"].get("temp_f", "No temperature found.")
+			weather = data["current_observation"].get("weather", "No weather found.")
+			loc = data["location"].get("city", "no city found")
+			state = data["location"].get("state", "no city found")
+			await self.bot.say("Current weather in " + str(loc) + ", " + str(state) + " is: " + str(weather) + " with a temperature of " + str(tempc) +" °C (" + str(tempf) + " °F).")
+		else:
+			await self.bot.say("Use the format: City,Country(or State/Province instead of Country) or Zip Code/Postal Code /// Any spaces in the location and the command will fail (EX: New York,NY will not work. Use a zip/postal code instead. EX: 10001)")
+	
+	@commands.command(no_pm=True, pass_context=False)
+	async def forecast(self, location):
+		"""Get a forecast link for the location.
+		\nMake sure to get your own API key and put it into data/weather/settings.json\nYou can get an API key from: www.wunderground.com/weather/api/"""
+		url = "http://api.wunderground.com/api/" + self.settings['api_key'] + "/geolookup/conditions/q/" + str(location) + ".json"
+		async with aiohttp.get(url) as r:
+			data = await r.json()
+		
+		if "current_observation" in data:
+			fore = data["current_observation"].get("forecast_url", "No forecast found.")
+			loc = data["location"].get("city", "no city found")
+			state = data["location"].get("state", "no city found")
+			await self.bot.say("Current forecast for " + str(loc) + ", " + str(state) + " is: " + str(fore) + ".")
+		else:
+			await self.bot.say("Use the format: City,Country(or State/Province instead of Country) or Zip Code/Postal Code /// Any spaces in the location and the command will fail (EX: New York,NY will not work. Use a zip/postal code instead. EX: 10001)")
+	
+	@commands.command(no_pm=True, pass_context=False)
+	async def nextday(self, location):
+		"""Get the weather for tomorrow.
+		\nMake sure to get your own API key and put it into data/weather/settings.json\nYou can get an API key from: www.wunderground.com/weather/api/"""
+		url = "http://api.wunderground.com/api/" + self.settings['api_key'] + "/geolookup/forecast/q/" + str(location) + ".json"
+		async with aiohttp.get(url) as r:
+			data = await r.json()
+
+		if "location" in data:
+			loc = data["location"].get("city", "no city found")
+			state = data["location"].get("state", "no state found")
+			tomorrow = data["forecast"]["txt_forecast"]["forecastday"]
+
+			for period in tomorrow:
+					if period["period"] == 2:
+						metric = period.get("fcttext_metric", "No metric found")
+						day = period.get("title", "No day found")
+						await self.bot.say("The weather for " + str(loc) + ", " + str(state) + " on " + str(day) + " is: " + str(metric))
+		else:
+			await self.bot.say("Use the format: City,Country(or State/Province instead of Country) or Zip Code/Postal Code /// Any spaces in the location and the command will fail (EX: New York,NY will not work. Use a zip/postal code instead. EX: 10001)")
+	
+	@commands.command(no_pm=True, pass_context=False)
+	async def nextnight(self, location):
+		"""Get the weather for tomorrow night.
+		\nMake sure to get your own API key and put it into data/weather/settings.json\nYou can get an API key from: www.wunderground.com/weather/api/"""
+		url = "http://api.wunderground.com/api/" + self.settings['api_key'] + "/geolookup/forecast/q/" + str(location) + ".json"
+		async with aiohttp.get(url) as r:
+			data = await r.json()
+
+		if "location" in data:
+			loc = data["location"].get("city", "no city found")
+			state = data["location"].get("state", "no state found")
+			tomorrow = data["forecast"]["txt_forecast"]["forecastday"]
+
+			for period in tomorrow:
+					if period["period"] == 3:
+						metric = period.get("fcttext_metric", "No metric found")
+						day = period.get("title", "No day found")
+						await self.bot.say("The weather for " + str(loc) + ", " + str(state) + " on " + str(day) + " is: " + str(metric))
+		else:
+			await self.bot.say("Use the format: City,Country(or State/Province instead of Country) or Zip Code/Postal Code /// Any spaces in the location and the command will fail (EX: New York,NY will not work. Use a zip/postal code instead. EX: 10001)")
+	
 def check_folders():
 	if not os.path.exists("data/weather"):
 		print("Creating data/weather folder...")
